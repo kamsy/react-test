75a680337666b5f60bfb78730c0eac3636bfc202
Kamsy <olekakamsy@gmail.com>
2019-05-17 07:46:20 +0100

    finished working on logic to fetch and render the results of the fetch, display the loader

--- public/App.js
+++ public/App.js
@@ -1,42 +1,37 @@
+const { Component, Fragment } = React;
+
 const e = React.createElement;
 
-const LoadingPage = ({ text }) => {
-    return ( <
-        div className = "loading-container" >
-        <
-        div className = "spinner" >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        div / >
-        <
-        /div> <
-        p > { text } < /p> <
-        /div>
+const LoadingPage = () => {
+    return (
+        <div className="dot">
+            Loading
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
+        </div>
     );
 };
 
-class App extends React.Component {
+const formatMoney = (amount, decimalCount = 2, decimal = ".") => {
+    try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+        // to convert the asprice to a string
+        let i = amount.toString();
+
+        let y = i.length > 2 ? i.length % 2 : 0;
+        return (
+            "$" +
+            ((y ? i.substring(0, y) : 0) +
+                (decimalCount ? decimal + i.substring(y) : ""))
+        );
+    } catch (e) {
+        console.log("TCL: e", e);
+    }
+};
+
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -44,7 +39,8 @@ class App extends React.Component {
             loading: false,
             currentPage: 1,
             error: "",
-            hasMore: true
+            hasMore: true,
+            dropDown: false
         };
 
         // the scroll function to fetch data...
@@ -52,7 +48,7 @@ class App extends React.Component {
             const {
                 fetchProducts,
                 incrementPage,
-                state: { error, loading, hasMore, currentPage }
+                state: { error, loading, hasMore }
             } = this;
 
             // Bails early if:
@@ -63,9 +59,9 @@ class App extends React.Component {
 
             // Checks that the page has scrolled close to the bottom
             const { offsetHeight, scrollTop } = document.documentElement;
-            console.log(window.innerHeight, "innerHeight");
-            console.log(offsetHeight, "offsetHeight");
-            console.log(scrollTop, "scrollTop");
+            // console.log(window.innerHeight, "innerHeight");
+            // console.log(offsetHeight, "offsetHeight");
+            // console.log(scrollTop, "scrollTop");
             if (offsetHeight - window.innerHeight === scrollTop) {
                 incrementPage();
                 fetchProducts();
@@ -77,29 +73,17 @@ class App extends React.Component {
         this.fetchProducts();
     }
 
-    componentDidUpdate() {
-        console.log(
-            "TCL: App -> componentDidUpdate -> this.state.currentPage",
-            this.state.currentPage
-        );
-        console.log("TCL: App -> componentDidUpdate -> this.state", this.state);
-    }
-
     fetchProducts = () => {
         const { currentPage } = this.state;
-        console.log("TCL: App -> fetchProducts -> currentPage", currentPage);
         this.setState({ loading: true }, () => {
-            fetch(`/api/products?_page=${currentPage}&_limit=60`)
+            fetch(`/api/products?_page=${currentPage}&_limit=250`)
                 .then(res => res.json())
                 .then(result => {
-                    console.log(
-                        "TCL: App -> componentDidMount -> result",
-                        result
-                    );
                     return this.setState(prevState => {
                         return {
                             data: [...prevState.data, ...result],
-                            loading: false
+                            loading: false,
+                            hasMore: result.length > 0 ? true : false
                         };
                     });
                 })
@@ -114,34 +98,45 @@ class App extends React.Component {
 
     incrementPage = () => {
         this.setState(prevState => {
-            return {...prevState, currentPage: prevState.currentPage + 1 };
+            return { ...prevState, currentPage: prevState.currentPage + 1 };
+        });
+    };
+
+    openSorters = () => {
+        this.setState(prevState => {
+            return { ...prevState, dropDown: !prevState.dropDown };
         });
     };
 
     render() {
-        const { loading, data } = this.state;
-        console.log("TCL: App -> render -> loaded", loading);
-        if (!loading) {
-            return ( <
-                div className = "grid-cont" > {
-                    data.map(datum => {
+        const { loading, data, hasMore } = this.state;
+        console.log("TCL: render -> hasMore", hasMore);
+
+        return (
+            <Fragment>
+                <div className="grid-cont">
+                    {data.map(datum => {
                         const { size, price, face, id } = datum;
-                        return ( <
-                            div className = "grid-item"
-                            key = { id } >
-                            <
-                            span > Size: { size } < /span> <
-                            span > Price: { price } < /span> <
-                            span > Face: { face } < /span> <
-                            /div>
+                        return (
+                            <div className="grid-item" key={id}>
+                                <span
+                                    style={{ fontSize: `${size}px` }}
+                                    className="face">
+                                    {face}
+                                </span>
+                                <hr />
+                                <span className="money">
+                                    {formatMoney(price)}{" "}
+                                </span>
+                                <span> Size: {size} </span>
+                            </div>
                         );
-                    })
-                } <
-                /div>
-            );
-        } else {
-            return <LoadingPage text = "loading..." / > ;
-        }
+                    })}
+                </div>
+                {loading ? <LoadingPage /> : null}
+                {hasMore ? null : "~ end of catalogue ~"}
+            </Fragment>
+        );
     }
 }
 
